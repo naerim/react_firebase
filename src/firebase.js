fd9233a9d702f5df0a55763cc9a3da557c69a8a5
naerim <naerim1119@gmail.com>
2020-12-02 23:18:52 +0900

    [SET] firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyuC_LYLm-TOQezmTWFn3tKCWdKD7urK4",
+  authDomain: "memoweb-aad79.firebaseapp.com",
+  databaseURL: "https://memoweb-aad79.firebaseio.com",
+  projectId: "memoweb-aad79",
+  storageBucket: "memoweb-aad79.appspot.com",
+  messagingSenderId: "24795862141",
+  appId: "1:24795862141:web:a4533d7ca4d36fc4494d14",
+  measurementId: "G-PE0H4D23YL"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
